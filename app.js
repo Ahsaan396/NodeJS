65338fb1f05daf5a2f82a03bb9658e36dba484fe
Ahsanul Hakim <ahsanulhakim285@gmail.com>
2021-06-01 14:50:56 +0600

    fsModule

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
 var http=require('http');
-var url = require('url');
-const { type } = require('os');
-var server=http.createServer(function(req,res){
+var fs = require('fs');
+// const { type } = require('os');
+// var server=http.createServer(function(req,res){
   
-      if(req.url=="/")
-      {
-         res.writeHead(200,{'Content-Type':'text/html'})
-         res.write('<h2>This is HOME page</h2>')
-         res.end();
-      }
-      else if(req.url=="/about")
-      {
-         res.writeHead(200,{'Content-Type':'text/html'})
-         res.write('<h1>This is ABOUT page</h1>')
-         res.end();
-      }
-      else if(req.url=="/contact")
-      {
-         res.writeHead(200,{'Content-Type':'text/html'})
-         res.write('<h1>This is CONTACT page</h1>')
-         res.end();
-      }
-});
-server.listen(5050);
-console.log("Server run successfully");
-
+//       if(req.url=="/")
+//       {
+//          res.writeHead(200,{'Content-Type':'text/html'})
+//          res.write('<h2>This is HOME page</h2>')
+//          res.end();
+//       }
+//       else if(req.url=="/about")
+//       {
+//          res.writeHead(200,{'Content-Type':'text/html'})
+//          res.write('<h1>This is ABOUT page</h1>')
+//          res.end();
+//       }
+//       else if(req.url=="/contact")
+//       {
+//          res.writeHead(200,{'Content-Type':'text/html'})
+//          res.write('<h1>This is CONTACT page</h1>')
+//          res.end();
+//       }
+// });
 
---
+++ fsModule.js
@@ -0,0 +1,18 @@
+var http=require('http');
+var fs = require('fs');
+var server=http.createServer(function(req,res){
+  
+    if(req.url=="/")
+    {
+        fs.readFile('Home.html',function(error,data){
+             
+            res.writeHead(200,{'Content-Type':'text/html'})
+            res.write(data);
+            res.end();
+        });
+
+       
+    }
+ });
+server.listen(5050);
+console.log("Server run successfully");
