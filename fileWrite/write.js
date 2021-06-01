cff27b84d1b9ca8b0ff831982e127c4fb469600f
Ahsanul Hakim <ahsanulhakim285@gmail.com>
2021-06-01 18:05:07 +0600

    ascRenameFile

--- fileWrite/write.js
+++ fileWrite/write.js
@@ -1,26 +1,22 @@
-var http=require('http');
+var http = require('http');
 var fs = require('fs');
-var server=http.createServer(function(req,res){
-  
-    if(req.url=="/")
-    {
-        fs.writeFile('demo.txt','update.txt',function(error){
-             if(error){
-            res.writeHead(200,{'Content-Type':'text/html'})
-            res.write("File rename fail");
-            res.end();
-    }
-    else
-    {
-        res.writeHead(200,{'Content-Type':'text/html'})
-        res.write("File rename successfully");
-        res.end(); 
-    }
-        });
+var server = http.createServer(function (req, res) {
 
-       
+    if (req.url == "/") {
+        fs.rename('demo.txt', 'upd.txt', function (error) {
+            if (error) {
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.write("File rename fail");
+                res.end();
+            }
+            else {
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.write("File rename successfully");
+                res.end();
+            }
+        });
     }
- });
-server.listen(5050);
+});
+server.listen(4040);
 console.log("Server run successfully");
 // okay when you run it in browser then 1stly u see "File rename successfully" then again u refresh it an see "File rename fail" bcz after hitting 1st time it .txt will update and second time it will not match.. Happy coding:)
