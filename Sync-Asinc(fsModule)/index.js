2ee19df8a4ca535e255d52ff291277363ac49940
Ahsanul Hakim <ahsanulhakim285@gmail.com>
2021-06-01 16:02:50 +0600

    Synchronise

---
+++ Sync-Asinc(fsModule)/index.js
@@ -0,0 +1,15 @@
+var http=require('http');
+var fs = require('fs');
+var server=http.createServer(function(req,res){
+  
+    if(req.url=="/")
+    {
+        let myData = fs.readFileSync('./home1.html');
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(myData);
+        res.end();
+       
+    }
+ });
+server.listen(4040);
+console.log("Server run successfully");
