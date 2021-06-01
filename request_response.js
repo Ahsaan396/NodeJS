81b1e51ce1546cfdc36fbacb6f13a06c3a6ddeca
Ahsanul Hakim <ahsanulhakim285@gmail.com>
2021-06-01 15:02:41 +0600

    server

---
+++ request_response.js
@@ -0,0 +1,27 @@
+var http=require('http');
+var fs = require('fs');
+const { type } = require('os');
+var server=http.createServer(function(req,res){
+  
+      if(req.url=="/")
+      {
+         res.writeHead(200,{'Content-Type':'text/html'})
+         res.write('<h2>This is HOME page</h2>')
+         res.end();
+      }
+      else if(req.url=="/about")
+      {
+         res.writeHead(200,{'Content-Type':'text/html'})
+         res.write('<h1>This is ABOUT page</h1>')
+         res.end();
+      }
+      else if(req.url=="/contact")
+      {
+         res.writeHead(200,{'Content-Type':'text/html'})
+         res.write('<h1>This is CONTACT page</h1>')
+         res.end();
+      }
+});
+
+server.listen(5050);
+console.log("Server run successfully");
